53f3eda4f575b8c268d7d94241901216383abac7
agent_tim <agent_tim@mail.ru>
2022-04-19 18:54:01 +0500

    20220419_2

--- src/main/java/letscode/sarafan/controller/MessageController.java
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,11 +1,14 @@
 package letscode.sarafan.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import letscode.sarafan.domain.Message;
+import letscode.sarafan.domain.Views;
 import letscode.sarafan.repo.MessageRepo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -19,17 +22,20 @@ public class MessageController {
     }
 
     @GetMapping
+    @JsonView(Views.IdName.class)
     public List<Message> list() {
         return messageRepo.findAll();
     }
 
     @GetMapping("{id}")
+    @JsonView(Views.FullMessage.class)
     public Message getOne(@PathVariable("id") Message message) {
         return message;
     }
 
     @PostMapping
     public Message create(@RequestBody Message message) {
+        message.setCreationDate(LocalDateTime.now());
         return messageRepo.save(message);
     }
 
