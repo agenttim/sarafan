de8fa6a04281cb3a08da8380dfc5c5214f6f5727
agent_tim <agent_tim@mail.ru>
2022-04-29 20:01:35 +0500

    Add webpack

--- src/main/java/letscode/sarafan/controller/MainController.java
+++ src/main/java/letscode/sarafan/controller/MainController.java
@@ -3,6 +3,7 @@ package letscode.sarafan.controller;
 import letscode.sarafan.domain.User;
 import letscode.sarafan.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,20 +17,23 @@ import java.util.HashMap;
 public class MainController {
     private final MessageRepo messageRepo;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @Autowired
     public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
     }
 
     @GetMapping
-    public String main (Model model, @AuthenticationPrincipal User user) {
-
+    public String main(Model model, @AuthenticationPrincipal User user) {
         HashMap<Object, Object> data = new HashMap<>();
 
         data.put("profile", user);
         data.put("messages", messageRepo.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
 
         return "index";
     }
